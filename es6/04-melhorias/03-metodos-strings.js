e4704c4895dc2daef48e93de925908d2a0ad3477
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2017-12-09 23:59:54 -0200

    melhorias no ES6 conclude

---
+++ es6/04-melhorias/03-metodos-strings.js
@@ -0,0 +1,7 @@
+let text = 'lorem ipsum dolor sit amet';
+
+
+console.log(text.startsWith('lorem'));
+console.log(text.endsWith('amet'));
+console.log(text.includes('dolor'));
+console.log('lol '.repeat(3));
