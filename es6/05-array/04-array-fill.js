7975d9916e772ba7610ba356bc51b1d755789526
Mário Antônio do Amaral Rodeghiero <mario.rodeghiero@gmail.com>
2017-12-29 23:00:32 -0200

    started destructuring

---
+++ es6/05-array/04-array-fill.js
@@ -0,0 +1,14 @@
+// Interessante para usar onde necessita de dados pré definidos.
+
+const arr = new Array(10);
+
+arr.fill("lol", 3, 6);
+
+console.log(arr)
+
+
+const newArr = [1, 2, 3, 4, 5, 6];
+
+newArr.fill("lol", 1, 3);
+
+console.log(newArr);
