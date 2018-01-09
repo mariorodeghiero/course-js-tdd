8ab9723e846ed9dc5ce8b1d767fd2c2f42ecd683
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-01-08 23:42:30 -0200

    class and prototype

---
+++ es6/09-classes/01-heranca-prototipal.js
@@ -0,0 +1,12 @@
+// class
+function Animal(kind, sound) {
+  // constructor
+  this.kind = kind;
+  this.sound = sound;
+}
+// method
+Animal.prototype.hello = function () {
+  console.log(`${this.sound} I'm a ${this.kind}!`);
+}
+const dog = new Animal('dog', 'auau');
+const cat = new Animal('cat', 'meow');
