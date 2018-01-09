a3f4a82092902abcb4c8f4184c43f2246675042b
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-01-08 23:24:25 -0200

    promises multiplas

--- es6/08-promises/03-promises.js
+++ es6/08-promises/03-promises.js
@@ -1,3 +1,18 @@
-/*
-*teste user new
-*/
+const currency = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({ currency: 'euro', value: 3.50 });
+  }, 5000);
+});
+const countries = new Promise((resolve, reject) => {
+  resolve(['Ireland', 'England', 'Scotland']);
+});
+// Promise
+//     .all([currency, countries])
+//     .then(responses => {
+//         console.log(responses);
+//     })
+Promise
+  .race([currency, countries])
+  .then(responses => {
+    console.log(responses);
+  })
