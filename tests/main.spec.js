ce5b3204155ab9a4701610221764c36995d429c4
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-04-05 20:32:52 -0300

    commands skip, only, bail

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,19 +1,17 @@
-describe("Main", function () {
-  describe("Method A", function () {
-    context("Case A", function () {
-      it("should happen test-1", function () {});
-    });
-
-    context("Case B", function () {
-      it("should happen test-2", function () {
-
+describe("Main", function() {
+  describe("Method A", function() {
+    context("Case A", function() {
+      it("should happen test-1", function() {
+        throw new Error("just on error");
       });
+    });
 
-      it("should happen test-3", function () {});
+    context.only("Case B", function() {
+      it("should happen test-2", function() {});
+
+      it("should happen test-3", function() {});
     });
   });
 
-  describe("Method B", function () {
-
-  });
+  describe("Method B", function() {});
 });
