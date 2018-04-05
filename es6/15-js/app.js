de206e73e00daac4d1803372a90fc84abebaea48
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-04-04 21:58:56 -0300

    start TDD

--- es6/15-js/app.js
+++ es6/15-js/app.js
@@ -1,13 +1,9 @@
 import * as R from "ramda";
 import sum from "./utils";
-import {
-  sub,
-  multiplicacao,
-  div
-} from "./utils";
+import { sub, multiplicacao, div } from "./utils";
 
-import react from 'react';
-import reactDOM from 'react-dom';
+import react from "react";
+import reactDOM from "react-dom";
 
 const arr1 = [1, 3, 1, 3, 1, 3, 1, 3];
 const arr2 = [1, 2, 9, 3, 9, 3, 9, 3];
@@ -19,7 +15,7 @@ console.log("------------------------------------");
 console.log(arr3);
 console.log(arr4);
 console.log(sum(2, 4));
-console.log(sub(2, 4));
+console.log(sub(99, 4));
 console.log(multiplicacao(2, 4));
 console.log(div(2, 4));
 console.log("------------------------------------");
