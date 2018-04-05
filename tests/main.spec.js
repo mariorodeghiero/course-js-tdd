6042f7340c489daeea6ba772bdf0a361929a9a77
mariorodeghiero <mario.rodeghiero@gmail.com>
2018-04-05 19:55:44 -0300

    created file main.espec.js

---
+++ tests/main.spec.js
@@ -0,0 +1,13 @@
+describe("main", function() {
+  describe("Method A", function() {
+    context("Case 1", function() {
+      it("should happen ");
+    });
+  });
+
+  context("Case B", function() {
+    it("should happen");
+  });
+
+  describe("Method B", function() {});
+});
